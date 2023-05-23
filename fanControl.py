c8b2ccc112335a6908388c07d8f8a585dfe410dd
Ben Radey <benjamin.radey+gpg@gmail.com>
2023-05-23 13:41:54 -0400

    Moved ipmitool to network call vs raw device, also looping through sensors now

--- fanControl.py
+++ fanControl.py
@@ -8,32 +8,59 @@ MAX_FAN = 100
 
 CPU_LOW = 50
 CPU_HIGH = 80
+thresholdMap = dict({
+    'CPU1': [50, 80],
+    'CPU2': [50, 80],
+    'System': [45, 72],
+    'Peripheral': [45, 72],
+    'MB_10G': [75, 88],
+    'MB/AOM_SAS': [65, 88]
+})
 
-IPMI_COMMAND_PREFIX = 'ipmitool raw 0x30 0x70 0x66 0x01 '
+IPMI_COMMAND_PREFIX = 'ipmitool -H 192.168.1.160 -U ADMIN -f /root/ipmiPassword -I lanplus '
+IPMI_SENSOR_COMMAND = IPMI_COMMAND_PREFIX + 'sensor'
+IPMI_FAN_COMMAND_PREFIX = IPMI_COMMAND_PREFIX + 'raw 0x30 0x70 0x66 0x01 '
+
+print(thresholdMap)
 
 while True:
-    stream = os.popen('ipmitool sensor')
+    stream = os.popen(IPMI_SENSOR_COMMAND)
+    readings = dict()
+    maxSetspeed = -1
+    hottestDevice = ['', -1]
     for line in stream.readlines():
-        if 'degrees C' in line and 'CPU2' in line:
-            #        print(line)
-            arr = line.split()
-            name = arr[0]
-            temp = float(arr[3])
-            limit = float(arr[16])
-            setspeed = MAX_FAN
-            print(name + ' ' + str(temp) + '/' + str(limit))
-            if temp < CPU_LOW:
-                print('Temp below CPU_LOW')
-                setspeed = MIN_FAN
-            elif temp < CPU_HIGH:
-                setspeed = float((temp - CPU_LOW) / (CPU_HIGH - CPU_LOW) * 100)
-                setspeed = setspeed + (20 * (100 - setspeed) / 100) 
-                print('setspeed: ' + str(setspeed))
-            command = IPMI_COMMAND_PREFIX + '0 ' + str(int(setspeed))
-            print(command)
-            os.system(command)
-            command = IPMI_COMMAND_PREFIX + '1 ' + str(int(setspeed))
-            print(command)
-            os.system(command)
+        if not 'degrees' in line:
+            continue
+        arr = line.split()
+        name = arr[0]
+        temp = float(arr[3])
+        limit = float(arr[16])
+        readings[name] = temp
+    print(readings)
+    for name in thresholdMap:
+        temp = readings[name]
+        lowTemp = thresholdMap[name][0]
+        highTemp = thresholdMap[name][1]
+        setspeed = MAX_FAN
+        print(name + ': ' + str(temp) + ' :: ' + str(lowTemp) + '-' + str(highTemp))
+        if temp < lowTemp:
+#            print('Temp below CPU_LOW')
+            setspeed = MIN_FAN
+        elif temp < highTemp:
+            setspeed = float((temp - lowTemp) / (highTemp - lowTemp) * 100)
+            setspeed = setspeed + (20 * (100 - setspeed) / 100) 
+#            print('setspeed: ' + str(setspeed))
+        setspeed = int(setspeed)
+        print(name + ' setspeed: ' + str(setspeed))
+        if setspeed > maxSetspeed:
+            hottestDevice = [name, temp]
+        maxSetspeed = max(maxSetspeed, setspeed)
+    print('Final setspeed: ' + str(maxSetspeed) + ' due to ' + hottestDevice[0] + ': ' + str(hottestDevice[1]))
+    command = IPMI_FAN_COMMAND_PREFIX + '0 ' + str(maxSetspeed)
+    print(command)
+    os.system(command)
+    command = IPMI_FAN_COMMAND_PREFIX + '1 ' + str(maxSetspeed)
+    print(command)
+    os.system(command)
     time.sleep(2)
 
